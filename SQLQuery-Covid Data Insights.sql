12175cbe884fdf047821ae5ee3f321bf68ea35f6
abrownatx <40504163+abrownatx@users.noreply.github.com>
2023-07-10 12:24:16 -0500

    Update SQLQuery-Covid Data Insights.sql

--- SQLQuery-Covid Data Insights.sql
+++ SQLQuery-Covid Data Insights.sql
@@ -1,46 +1,40 @@
 /* We will be querying covid data from the "Our World in Data" website. The Covid-19 Deaths dataset was downloaded on July 5, 2023  */
 
+--1) Lets take a look at the data and review its design.
+
 SELECT *
 FROM PortfolioProject..CovidDeaths
 Where continent is not null
 ORDER BY 3,4
 
---SELECT *
---FROM PortfolioProject..CovidVaccinations
---ORDER BY 3, 4
-
---Overview of the data that we will be using for our analysis
-
-SELECT location, date, CAST(total_cases AS numeric), CAST(total_deaths AS numeric),(CAST(total_deaths AS numeric)/CAST(total_cases AS numeric))*100 DeathPercentage 
-FROM PortfolioProject..CovidDeaths
-Where location like 'United States'
-ORDER by 3, 4
-
---Simple Calculation looking at Total Cases vs Total Deaths. Calculate the percentage and expressed as "DeathPercentage"
+--2) Simple Calculation looking at Total Cases vs Total Deaths. Calculate the percentage and expressed as "DeathPercentage". (Global data)
 
 SELECT continent, date, total_cases, total_deaths,(CAST(total_deaths AS numeric)/CAST(total_cases AS numeric))*100 DeathPercentage 
 FROM PortfolioProject..CovidDeaths
 Where continent is not null
 ORDER by 3, 4
 
---As of July 2023, the liklihood of dying from contracting Covid is roughly 1.1% according to United States data.
---As of July 5th,2023, 1,127,152 Americans have died from Covid 
+--3) Simple Calculation looking at Total Cases vs Total Deaths. Calculate the percentage and expressed as "DeathPercentage". (United States)
 
-SELECT location, date, CAST(total_cases AS numeric), CAST(total_deaths AS numeric),(CAST(total_deaths AS numeric)/CAST(total_cases AS numeric))*100 DeathPercentage 
+SELECT location, date, total_cases, total_deaths,(CAST(total_deaths AS numeric)/CAST(total_cases AS numeric))*100 DeathPercentage 
 FROM PortfolioProject..CovidDeaths
 Where location like 'United States'
-ORDER by 3, 4
+ORDER by 3, 4 desc
 
---Looking at the Total Cases vs Population with the calculation expressed as "CaseRate" also known as incidence rate.
---As of July 5th,2023, the percentage of Covid in the US is 30.58%.
+--As of July 2023, the liklihood of dying from contracting Covid in the U.S. is roughly 1.1%.
+--As of July 5th,2023, 1,127,152 Americans have died from Covid 
+
+
+--4) Looking at the Total Cases vs Population with the calculation expressed as "CaseRate" also known as incidence rate.
 
 SELECT location, date, population, CAST(total_cases AS numeric), (CAST(total_cases AS numeric)/CAST(population AS numeric))*100 PercentPopulationInfected
 FROM PortfolioProject..CovidDeaths
 Where location like 'United States'
 ORDER by 3, 4 desc
 
---What countries have the highest infection rates?
---The Top 5 countries for infection rates are Cyprus (73.76%), San Marino (72.21%), Brunei (68.67%), Austria(68.03%), and Faeroe Islands(65.25%).
+--As of July 5th,2023, the percentage of Covid in the US is 30.58%.
+
+--5) What countries have the highest infection rates?
 
 SELECT location, population, MAX(CAST(total_cases AS numeric)) AS HighestInfectionCount, MAX(CAST(total_cases AS numeric)/(CAST(population AS numeric)))*100 PercentPopulationInfected
 FROM PortfolioProject..CovidDeaths
@@ -49,12 +43,9 @@ Where continent is not null
 GROUP by location, population
 ORDER by PercentPopulationInfected desc
 
---Showing the countries with the highest reported death count per population
---United States	1127152
---Brazil	703964
---India	    531908
---Russia	399649
---Mexico	334336
+--The Top 5 countries for infection rates are Cyprus (73.76%), San Marino (72.21%), Brunei (68.67%), Austria(68.03%), and Faeroe Islands(65.25%).
+
+--6) Showing the countries with the highest reported death count per population
 
 SELECT location, MAX(CAST(total_deaths AS numeric)) AS TotalDeathCount
 FROM PortfolioProject..CovidDeaths
@@ -63,7 +54,21 @@ Where continent is not null
 GROUP by location
 ORDER by TotalDeathCount desc
 
---Lets break the data down by global stats:
+--United States	1127152
+--Brazil	703964
+--India	    531908
+--Russia	399649
+--Mexico	334336
+
+
+--7) Lets break the data down by global stats:
+
+SELECT location, MAX(CAST(total_deaths AS numeric)) AS TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+--WHERE location like 'United States'
+Where continent is null
+GROUP by location
+ORDER by TotalDeathCount desc
 
 --World Count:
 --6948751
@@ -84,26 +89,7 @@ ORDER by TotalDeathCount desc
 --Low Income 47958
 
 
-SELECT location, MAX(CAST(total_deaths AS numeric)) AS TotalDeathCount
-FROM PortfolioProject..CovidDeaths
---WHERE location like 'United States'
-Where continent is null
-GROUP by location
-ORDER by TotalDeathCount desc
-
---Global Data
-
-SELECT location, MAX(CAST(total_deaths AS numeric)) AS TotalDeathCount
-FROM PortfolioProject..CovidDeaths
---WHERE location like 'United States'
-Where continent is null
-GROUP by location
-ORDER by TotalDeathCount desc
-
---This data gives us an idea of new cases and deaths in the world
---Note, to avoid places in data that threw an error due to division by 0.
---With both ARITHABORT and ANSI_WARNINGS set to OFF, SQL Server will return a NULL value in a calculation involving a divide-by-zero error. 
---To return a 0 value instead of a NULL value, you can put the division operation inside an ISNULL function:
+--8) This data gives us an idea of new cases and deaths in the world by date.
 
 SET ARITHABORT OFF
 SET ANSI_WARNINGS OFF
@@ -113,11 +99,15 @@ FROM PortfolioProject..CovidDeaths
 --Where location like 'United States'
 Where continent  is not null
 group by date
-order by date desc
+order by date 
 
--- We are going to join both data sets we created, and aliased the tables as CovidDeaths = dea, and CovidVaccinations = vac
+--Note, to avoid places in data that threw an error due to division by 0.
+--With both ARITHABORT and ANSI_WARNINGS set to OFF, SQL Server will return a NULL value in a calculation involving a divide-by-zero error. 
+--To return a 0 value instead of a NULL value, you can put the division operation inside an ISNULL function:
+
+
+-- 9) We are going to join both data sets we created, and alias the tables as CovidDeaths = dea, and CovidVaccinations = vac
 --Next we will determine the total population vs vaccinations in the world
-
 --We will also include a rolling count of vaccinations by country and name this column, cummulativeVaccinations
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CAST(vac.new_vaccinations as numeric)) OVER (Partition by dea.location Order by dea.location, dea.date) AS CummulativeVaccinations
@@ -129,7 +119,7 @@ JOIN PortfolioProject..CovidVaccinations vac
 WHERE dea.continent is not null
 ORDER BY 1, 2, 3
 
---Here we will use CTE to look at vaccinations relative to the population
+--10) Here we will use CTE to look at vaccinations relative to the population
 
 With PopvsVac (Continent, location, date, population, New_vaccinations, CummulativeVaccinations)
 as
@@ -148,7 +138,7 @@ SELECT *, (CummulativeVaccinations/population)*100 AS PercentPopulationVaccinate
 FROM PopvsVac
 
 
---We will create a Table of PercentPopulationVaccinated using a temp table approach.
+--11) We will create a Table of PercentPopulationVaccinated using a temp table approach.
 
 DROP Table if exists #percentpopulationvaccinated
 CREATE Table #percentpopulationvaccinated
@@ -174,7 +164,7 @@ WHERE dea.continent is not null
 SELECT *, (CummulativeVaccinations/population)*100 AS PercentageVaccinated
 FROM #percentpopulationvaccinated
 
---Creating views to store data for Tableau Visulations
+--12) Creating views to store data for Tableau Visulations
 
 --View 1
 CREATE VIEW TotalDeathCount as
@@ -194,3 +184,6 @@ JOIN PortfolioProject..CovidVaccinations vac
 	and dea.date = vac.date
 WHERE dea.continent is not null
 --ORDER BY 1, 2, 3
+
+
+
